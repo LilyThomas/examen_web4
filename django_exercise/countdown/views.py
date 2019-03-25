4dada9a7932a23073e4552b034b351a12c093568
LilyThomas <anneliesthomas94@gmail.com>
2019-03-25 15:01:49 +0100

    timedifference + screenshots

--- django_exercise/countdown/views.py
+++ django_exercise/countdown/views.py
@@ -6,11 +6,19 @@ import datetime;
 
 def index(request):
     now = datetime.datetime.now();
+
+    # be is 1 uur later
     timediff = 1
 
-    brexit = datetime.datetime(2019, 3, 29, 23 - timediff)
+    # uk local
+    brexit = datetime.datetime(2019, 3, 29, 23)
 
-    diff = brexit - now
+    if(brexit.hour + timediff == 24):
+        countdown = datetime.datetime(2019,3,30)
+    else:
+        coundown = brexit
+
+    diff = countdown - now
 
     return render(request, 'countdown/index.html', {'timediff': timediff, 'date': diff, 'uk': brexit, 'now': now})
 
