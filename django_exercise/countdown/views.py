be18ab31b4ff451d89e65a65fb8923a566fedd65
LilyThomas <anneliesthomas94@gmail.com>
2019-03-25 14:34:58 +0100

    coundown finished + layout adjusted

--- django_exercise/countdown/views.py
+++ django_exercise/countdown/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
+import datetime;
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'countdown/index.html', {})
+    now = datetime.datetime.now();
+    timediff = 1
+
+    brexit = datetime.datetime(2019, 3, 29, 23 - timediff)
+
+    diff = brexit - now
+
+    return render(request, 'countdown/index.html', {'timediff': timediff, 'date': diff, 'uk': brexit, 'now': now})
 
